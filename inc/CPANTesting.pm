5efe276e549a95ab512e9bbaed4fe5bde549f89d
Peter Flanigan <pjfl@cpan.org>
2013-05-04 00:18:52 +0000

    Import of PJFL/Yakuake-Sessions-v0.4.2 from CPAN.
    gitpan-cpan-distribution: Yakuake-Sessions
    gitpan-cpan-version:      v0.4.2
    gitpan-cpan-path:         PJFL/yakuake-sessions/Yakuake-Sessions-v0.4.2.tar.gz
    gitpan-cpan-author:       PJFL
    gitpan-cpan-maturity:     released

--- inc/CPANTesting.pm
+++ inc/CPANTesting.pm
@@ -1,5 +1,5 @@
-# @(#)Ident: CPANTesting.pm 2013-03-27 23:42 pjf ;
-# Bob-Version: 1.8
+# @(#)Ident: CPANTesting.pm 2013-04-28 22:50 pjf ;
+# Bob-Version: 1.12
 
 package CPANTesting;
 
@@ -13,14 +13,17 @@ sub is_testing { !! ($ENV{AUTOMATED_TESTING} || $ENV{PERL_CR_SMOKER_CURRENT}
                  || ($ENV{PERL5OPT} || q()) =~ m{ CPAN-Reporter }mx) }
 
 sub should_abort {
+   is_testing() or return 0;
+
+   $host eq q(craig-aspire-5560) and return
+      "Stopped Treptow ${host} - 7ec294fe-b005-11e2-91e1-3039f74438b4";
    return 0;
 }
 
 sub test_exceptions {
    my $p = shift; is_testing() or return 0;
 
-   $p->{stop_tests} and return 'CPAN Testing stopped in Build.PL';
-
+   $p->{stop_tests}     and return 'CPAN Testing stopped in Build.PL';
    $osname eq q(mirbsd) and return 'Mirbsd  OS unsupported';
    return 0;
 }
