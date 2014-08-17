981263d00a61edf462bae019d1ea8c27a4cd7ba7
Peter Flanigan <pjfl@cpan.org>
2014-08-17 16:44:40 +0000

    Import of PJFL/Yakuake-Sessions-0.14.1 from CPAN.
    gitpan-cpan-distribution: Yakuake-Sessions
    gitpan-cpan-version:      0.14.1
    gitpan-cpan-path:         PJFL/yakuake-sessions/Yakuake-Sessions-0.14.1.tar.gz
    gitpan-cpan-author:       PJFL
    gitpan-cpan-maturity:     released

--- lib/Yakuake/Sessions/TraitFor/DBus.pm
+++ lib/Yakuake/Sessions/TraitFor/DBus.pm
@@ -1,12 +1,13 @@
 package Yakuake::Sessions::TraitFor::DBus;
 
-use namespace::sweep;
+use namespace::autoclean;
 
-use Class::Usul::Constants;
-use Class::Usul::Functions  qw( trim zip );
-use Class::Usul::Time       qw( nap );
-use Class::Usul::Types      qw( LoadableClass );
-use English                 qw( -no_match_vars );
+use Class::Usul::Constants qw( EXCEPTION_CLASS NUL SPC );
+use Class::Usul::Functions qw( throw trim zip );
+use Class::Usul::Time      qw( nap );
+use Class::Usul::Types     qw( LoadableClass Object );
+use English                qw( -no_match_vars );
+use Net::DBus::Annotation  qw( :call );
 use Moo::Role;
 
 requires qw( debug run_cmd );
@@ -14,20 +15,24 @@ requires qw( debug run_cmd );
 # Public attribuetes
 has 'dbus_class' => is => 'lazy', isa => LoadableClass, default => 'Net::DBus';
 
-has 'service'    => is => 'lazy', isa => sub { $_[ 0 ]->can( 'get_service' ) },
+has 'service'    => is => 'lazy',
+   isa           => sub { $_[ 0 ]->can( 'get_object' )
+                             or throw 'Attribute "service" cannot get_object' },
    default       => sub { $_[ 0 ]->dbus->get_service( 'org.kde.yakuake' ) },
    init_arg      => undef;
 
-has 'sessions'   => is => 'lazy', isa => sub { $_[ 0 ]->can( 'get_object' ) },
+has 'sessions'   => is => 'lazy', isa => Object,
    default       => sub { $_[ 0 ]->service->get_object( '/yakuake/sessions' ) },
    init_arg      => undef;
 
-has 'tabs'       => is => 'lazy', isa => sub { $_[ 0 ]->can( 'get_object' ) },
+has 'tabs'       => is => 'lazy', isa => Object,
    default       => sub { $_[ 0 ]->service->get_object( '/yakuake/tabs' ) },
    init_arg      => undef;
 
 # Private attributes
-has '_dbus'      => is => 'lazy', isa => sub { $_[ 0 ]->can( 'session' ) },
+has '_dbus'      => is => 'lazy',
+   isa           => sub { $_[ 0 ]->can( 'get_service' )
+                             or throw 'Attribute "dbus" cannot get_service' },
    default       => sub { $_[ 0 ]->dbus_class->session }, reader => 'dbus';
 
 # Public methods
@@ -45,13 +50,15 @@ sub apply_sessions {
       $self->log->debug( "Applying ${tab_no} ${sess_id} ${ksess_id} ${title}" );
 
       $self->set_tab_title_for_session( $title, $sess_id );
-      $tab->{cwd   } and $self->sessions->runCommand( 'cd '.$tab->{cwd} );
-      $tab->{cmd   } and $self->sessions->runCommand( $tab->{cmd} );
+      $tab->{cwd   } and $self->sessions->runCommand
+         ( dbus_call_sync, 'cd '.$tab->{cwd} );
+      $tab->{cmd   } and $self->sessions->runCommand
+         ( dbus_call_sync, $tab->{cmd} );
       $tab->{active} and $active = $sess_id;
       $tab_no++;
    }
 
-   defined $active and $self->sessions->raiseSession( $active );
+   defined $active and $self->sessions->raiseSession( dbus_call_sync, $active );
    return;
 }
 
@@ -84,7 +91,7 @@ sub set_tab_title_for_session {
 
    $sess_id //= $self->_get_active_session_id;
 
-   return $self->tabs->setTabTitle( $sess_id, $title );
+   return $self->tabs->setTabTitle( dbus_call_sync, $sess_id, $title );
 }
 
 # Private methods
@@ -100,7 +107,7 @@ sub _close_session {
 }
 
 sub _get_active_session_id {
-   return int $_[ 0 ]->sessions->activeSessionId;
+   return int $_[ 0 ]->sessions->activeSessionId( dbus_call_sync );
 }
 
 sub _get_current_directory {
@@ -130,7 +137,7 @@ sub _get_ksession_ids {
 }
 
 sub _get_session_at_tab {
-   return int $_[ 0 ]->tabs->sessionAtTab( $_[ 1 ] );
+   return int $_[ 0 ]->tabs->sessionAtTab( dbus_call_sync, $_[ 1 ] );
 }
 
 sub _get_session_fg_process_id {
@@ -138,9 +145,11 @@ sub _get_session_fg_process_id {
 }
 
 sub _get_session_ids {
+   my $sessions = $_[ 0 ]->sessions;
+
    return ( sort   { $a <=> $b }
             map    { int $_ }
-            split m{ , }msx, $_[ 0 ]->sessions->sessionIdList );
+            split m{ , }msx, $sessions->sessionIdList( dbus_call_sync ) );
 }
 
 sub _get_session_map {
@@ -156,7 +165,8 @@ sub _get_session_process_id {
 }
 
 sub _get_tab_title {
-  (my $title = $_[ 0 ]->tabs->tabTitle( $_[ 1 ] )) =~ s{ \A \d+ \s+ }{}mx;
+  (my $title = $_[ 0 ]->tabs->tabTitle( dbus_call_sync, $_[ 1 ] ) )
+     =~ s{ \A \d+ \s+ }{}mx;
 
    return $title;
 }
@@ -175,7 +185,7 @@ sub _maybe_add_session {
 
    $tab_no > 0 or return $sess_id;
 
-   my $old_id = $sess_id; $self->sessions->addSession;
+   my $old_id = $sess_id; $self->sessions->addSession( dbus_call_sync );
 
    while (not length $sess_id or $old_id == $sess_id) {
       nap $self->config->nap_time; $sess_id = $self->_get_active_session_id;
